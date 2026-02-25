0a93e44382be13160b17e7dc0256a15dba70b3e2
Osama Ragab <osamaragab182002-ai@gmail.com>
2026-02-25 10:55:06 +0200

    fix: pass secret to getToken in wishlist route

--- src/app/api/wishlist/route.ts
+++ src/app/api/wishlist/route.ts
@@ -4,7 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req :NextRequest){
     
-const token= await getToken({req})
+// السطر رقم 7 تقريباً
+const token = await getToken({ 
+    req, 
+    secret: process.env.NEXTAUTH_SECRET 
+});
 if(!token){
     return NextResponse.json({error:'Unauthorized' , status :401})
 }console.log("TOKEN 👉", token)
